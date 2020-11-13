1a180d3bd5aa787dc1b01205fa466f60d796a44b
Rafael Leonan <faelleonan@gmail.com>
2020-11-13 13:26:53 -0800

    Conexao com o banco

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+	_ "github.com/lib/pq"
 )
 
 type Produto struct {
@@ -14,7 +16,10 @@ type Produto struct {
 
 var views = template.Must(template.ParseGlob("views/*.html"))
 
+
 func main(){
+	db := conexaoBd()
+	defer db.Close()
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":5000", nil)
 }
@@ -24,7 +29,18 @@ func index(w http.ResponseWriter, r *http.Request){
 		{ Nome:"Camiseta", Descricao:"Azul", Preco:39, Quantidade: 3 },
 		{"Fone", "Preto", 25, 10 },
 		{"Headset", "Preto", 50, 10 },
+		{"Novo Produto", "Red", 20, 15 },
 	}
 
 	views.ExecuteTemplate(w, "Index", produtos)
+}
+func conexaoBd() *sql.DB {
+	conexao := "user=postgres dbname=alura_loja password=postgres host=localhost sslmode=disabled"
+	db, err := sql.Open("postgres", conexao)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
 }
