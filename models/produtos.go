e0890cee851275dbd6a49ad49072841674fa7914
Rafael Leonan <faelleonan@gmail.com>
2020-11-16 09:33:28 -0800

    Deletando produtos

--- models/produtos.go
+++ models/produtos.go
@@ -33,6 +33,7 @@ func BuscaTodosOsProdutos() []Produto {
 			panic(err.Error())
 		}
 
+		p.Id = id
 		p.Nome = nome
 		p.Descricao = descricao
 		p.Preco = preco
@@ -54,5 +55,19 @@ func CriarProduto(nome, descricao string, preco float64, quantidade int){
 	}
 
 	inseriDados.Exec(nome, descricao, preco, quantidade)
+	defer db.Close()
+}
+
+func DeletarProduto(id string){
+	db := datasource.ConexaoBd()
+
+	deletarProduto, err := db.Prepare("delete from produtos where id=$1")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	deletarProduto.Exec(id)
+
 	defer db.Close()
 }
